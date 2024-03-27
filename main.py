e76323e25a2dd74b9a8e94dcb74e6db458e00e86
Skobarev A.A <skobarev.aa@gmail.com>
2024-03-27 17:41:39 +0300

    new version

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from binance import Client
 from binance.enums import HistoricalKlinesType
 import json
 import datetime
+import time
 import src.logger as custom_logging
 from src.config_handler import TIMEFRAMES, BINANCE_API_KEY, BINANCE_Secret_KEY
+from send_all_signals import process_signal
 
 THREAD_CNT = 1  # 3 потока на ядро
 
@@ -47,81 +49,6 @@ def check_history_bars_for_pattern_3bars(pair, bars: list) -> str:
             f'{pair}: CLOSE: {cl[2]} < {cl[1]} < {cl[0]} VOL: {vol[2]} > {vol[1]} > {vol[0]} => LONG')
         return "LONG"
     return ""
-
-
-def check_history_bars_for_pattern_2bars_v2(pair, bars: list) -> str:
-    """
-    Поиск свечного паттерна в барах истории
-    :param bars:
-    :return:
-    """
-    if len(bars) < 3:
-        print(f'{pair}: Bar count = {len(bars)}.')
-        return ""
-    _time = []
-    cl = []
-    op = []
-    vol = []
-
-    for bar in bars:
-        value = datetime.datetime.fromtimestamp(bar[0] / 1000)
-        # print(value.strftime('%Y-%m-%d %H:%M:%S'))
-        _time.append(value.strftime('%Y-%m-%d %H:%M:%S'))
-        op.append(float(bar[1]))
-        cl.append(float(bar[4]))
-        vol.append(float(bar[5]))
-
-    # проверяем значения на паттерны
-    if cl[2] > cl[1] and vol[2] > vol[1] and cl[2] - op[2] > cl[1] - op[1] \
-            and cl[2] > op[2] and cl[1] > op[1]:
-        # цена и объемы росли росли 2 дня подряд, размер баров (close-open) увеличивался в течении этих 2-х дней
-        custom_logging.info(
-            f'{pair}: CLOSE: {cl[2]}- > {cl[1]}  VOL: {vol[2]} > {vol[1]}  => SHORT')
-        return "SHORT"
-    elif cl[2] < cl[1] and vol[2] > vol[1] and op[2] - cl[2] > op[1] - cl[1] \
-            and op[2] > cl[2] and op[1] > cl[1]:
-        # цена падала 2 дня подряд, а объемы росли росли 2 дня подряд, размер баров (open-close) увеличивался в течении этих 2-х дней
-        custom_logging.info(
-            f'{pair}: CLOSE: {cl[2]} < {cl[1]}  VOL: {vol[2]} > {vol[1]}   => LONG')
-        return "LONG"
-    return ""
-
-
-def check_history_bars_for_pattern_2bars_v3(pair, bars: list) -> str:
-    """
-    Поиск свечного паттерна в барах истории
-    :param bars:
-    :return:
-    """
-    if len(bars) < 3:
-        print(f'{pair}: Bar count = {len(bars)}.')
-        return ""
-    _time = []
-    cl = []
-    op = []
-    vol = []
-
-    for bar in bars:
-        value = datetime.datetime.fromtimestamp(bar[0] / 1000)
-        # print(value.strftime('%Y-%m-%d %H:%M:%S'))
-        _time.append(value.strftime('%Y-%m-%d %H:%M:%S'))
-        op.append(float(bar[1]))
-        cl.append(float(bar[4]))
-        vol.append(float(bar[5]))
-
-    # проверяем значения на паттерны
-    if cl[2] > cl[1] and vol[2] > vol[1] and cl[2] > op[2] and cl[1] > op[1]:
-        # цена и объемы росли росли 2 дня подряд, размер баров (close-open) не учитываем
-        custom_logging.info(
-            f'{pair}: CLOSE: {cl[2]}- > {cl[1]}  VOL: {vol[2]} > {vol[1]}  => SHORT')
-        return "SHORT"
-    elif cl[2] < cl[1] and vol[2] > vol[1] and op[2] > cl[2] and op[1] > cl[1]:
-        # цена падала 2 дня подряд, а объемы росли росли 2 дня подряд, размер баров не учитываем
-        custom_logging.info(
-            f'{pair}: CLOSE: {cl[2]} < {cl[1]}  VOL: {vol[2]} > {vol[1]}   => LONG')
-        return "LONG"
-    return ""
-
 
 
 def check_history_bars_for_pattern_2bars_v1(pair, bars: list) -> str:
@@ -198,10 +125,28 @@ def load_history_bars(task):
                 result[timeframe] = 0
                 continue
             # ------------ check for 3 different bar patterns ----------------------------------------------------------
-            result["signal_3bars"] = check_history_bars_for_pattern_3bars(pair, bars)
-            result["signal_2bars_v2"] = check_history_bars_for_pattern_2bars_v2(pair, bars)
-            result["signal_2bars_v1"] = check_history_bars_for_pattern_2bars_v1(pair, bars)
-            result["signal_2bars_v3"] = check_history_bars_for_pattern_2bars_v3(pair, bars)
+            check_result = check_history_bars_for_pattern_3bars(pair, bars)
+            if check_result == "SHORT":
+                result["3bars_growing_volumes_v1"] = "SHORT"
+            else:
+                result["3bars_growing_volumes_v1"] = ""
+
+            if check_result == "LONG":
+                result["3bars_growing_volumes_v2"] = "LONG"
+            else:
+                result["3bars_growing_volumes_v2"] = ""
+
+            check_result = check_history_bars_for_pattern_2bars_v1(pair, bars)
+            if check_result == "SHORT":
+                result["2bars_falling_volumes_v1"] = "SHORT"
+            else:
+                result["2bars_falling_volumes_v1"] = ""
+            if check_result == "LONG":
+                result["2bars_falling_volumes_v2"] = "LONG"
+            else:
+                result["2bars_falling_volumes_v2"] = ""
+
+
             # ----------------------------------------------------------------------------------------------------------
         return result
     except Exception as e:
@@ -212,7 +157,7 @@ def load_history_bars(task):
 def store_signals_to_file(signals_data: dict, pattern_name: str):
     with open(f"signals_{pattern_name}/{datetime.date.today().isoformat()}.txt", 'w', encoding='utf-8') as f:
         json.dump(signals_data, f, ensure_ascii=False, indent=4, separators=(',', ': '))
-        print('Signals (pattern 3bars) data  stored to file.')
+        print(f'Signals data stored to file "signals_{pattern_name}/{datetime.date.today().isoformat()}.txt".')
         custom_logging.info(
             f'New signals data stored to file "signals_{pattern_name}/{datetime.date.today().isoformat()}.txt".')
         custom_logging.info(
@@ -221,31 +166,31 @@ def store_signals_to_file(signals_data: dict, pattern_name: str):
 
 def load_futures_history_bars_end(responce_list):
     signals_2bars_v1 = dict()
+    signals_3bars_v1 = dict()
     signals_2bars_v2 = dict()
-    signals_2bars_v3 = dict()
-
-    signals_3bars = dict()
+    signals_3bars_v2 = dict()
 
     for responce in responce_list:
         id = responce['id']
         del responce['id']
-        if responce['signal_2bars_v1'] != '':
-            signals_2bars_v1[id] = responce['signal_2bars_v1']
 
-        if responce['signal_2bars_v2'] != '':
-            signals_2bars_v2[id] = responce['signal_2bars_v2']
+        if responce['2bars_falling_volumes_v1'] != '':
+            signals_2bars_v1[id] = responce['2bars_falling_volumes_v1']
 
-        if responce['signal_2bars_v3'] != '':
-            signals_2bars_v3[id] = responce['signal_2bars_v3']
+        if responce['2bars_falling_volumes_v2'] != '':
+            signals_2bars_v1[id] = responce['2bars_falling_volumes_v2']
 
-        if responce['signal_3bars'] != '':
-            signals_3bars[id] = responce['signal_3bars']
+        if responce['3bars_growing_volumes_v1'] != '':
+            signals_3bars_v1[id] = responce['3bars_growing_volumes_v1']
+
+        if responce['3bars_growing_volumes_v2'] != '':
+            signals_3bars_v2[id] = responce['3bars_growing_volumes_v2']
 
     try:
-        store_signals_to_file(signals_3bars, "3bars")
-        store_signals_to_file(signals_2bars_v1, "2bars_v1")
-        store_signals_to_file(signals_2bars_v2, "2bars_v2")
-        store_signals_to_file(signals_2bars_v3, "2bars_v3")
+        store_signals_to_file(signals_3bars_v1, "3bars_growing_volumes_v1")
+        store_signals_to_file(signals_2bars_v1, "2bars_falling_volumes_v1")
+        store_signals_to_file(signals_3bars_v2, "3bars_growing_volumes_v2")
+        store_signals_to_file(signals_2bars_v2, "2bars_falling_volumes_v2")
 
     except Exception as e:
         print("load_futures_history_bars_end exception:", e)
@@ -264,6 +209,17 @@ if __name__ == '__main__':
             pool.map_async(load_history_bars, tasks, callback=load_futures_history_bars_end)
             pool.close()
             pool.join()
+
+            cur_date = datetime.date.today().isoformat()
+
+            process_signal('signals_2bars_falling_volumes_v1', cur_date)
+            time.sleep(1)
+            process_signal('signals_2bars_falling_volumes_v2', cur_date)
+            time.sleep(1)
+            process_signal('signals_3bars_growing_volumes_v1', cur_date)
+            time.sleep(1)
+            process_signal('signals_3bars_growing_volumes_v2', cur_date)
+
     except Exception as ex:
         print("Load history bars exception:", ex)
         custom_logging.error(f"Load history bars exception: {ex}")
